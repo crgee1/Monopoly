75dbdd3eb849680444899a4938b234353159eba6
cgee1 <christopherrgee@gmail.com>
2019-12-29 22:31:12 -0800

    Updates

---
+++ precache-manifest.026734005f4878c05a62383e0cb8ea54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93639cef2ad9315e130db9392291adb",
+    "url": "/monopoly/index.html"
+  },
+  {
+    "revision": "109e47a21119f8edeb43",
+    "url": "/monopoly/static/css/main.29387a76.chunk.css"
+  },
+  {
+    "revision": "bcfd26423d9bef107565",
+    "url": "/monopoly/static/js/2.08cbf798.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monopoly/static/js/2.08cbf798.chunk.js.LICENSE"
+  },
+  {
+    "revision": "109e47a21119f8edeb43",
+    "url": "/monopoly/static/js/main.0d18ec00.chunk.js"
+  },
+  {
+    "revision": "be8affc41a76f6bd50d8",
+    "url": "/monopoly/static/js/runtime-main.70b7032f.js"
+  }
+]);
