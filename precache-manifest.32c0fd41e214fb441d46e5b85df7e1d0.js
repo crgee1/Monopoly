ef6fd2571b87bbd7e0ada9770bbb30ce9d97d422
cgee1 <christopherrgee@gmail.com>
2019-12-30 00:32:41 -0800

    Updates

---
+++ precache-manifest.32c0fd41e214fb441d46e5b85df7e1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ed9b72b7d0d870283839de3c978cfe",
+    "url": "/Monopoly/index.html"
+  },
+  {
+    "revision": "79577d004a173daf2041",
+    "url": "/Monopoly/static/css/main.6eda4150.chunk.css"
+  },
+  {
+    "revision": "bcfd26423d9bef107565",
+    "url": "/Monopoly/static/js/2.08cbf798.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monopoly/static/js/2.08cbf798.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79577d004a173daf2041",
+    "url": "/Monopoly/static/js/main.4118b0d9.chunk.js"
+  },
+  {
+    "revision": "9417c9b7f3bea1e9e2b3",
+    "url": "/Monopoly/static/js/runtime-main.35724b30.js"
+  }
+]);
