4fca59e926a8393b023f778abfa025008d65c739
cgee1 <christopherrgee@gmail.com>
2020-02-11 16:22:16 -0800

    Prevent accidental back button

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Board from '../board/board';
 
 export default function Game(props) {
     const [activePlayer, setActivePlayer] = useState(0);
-
+    
     const { players, setPlayers } = props;
-    // if (players.length === 1) alert(`${players[activePlayer].name} won monopoly!`)
+
+    useEffect(() => {
+        window.onbeforeunload = function () {
+            return 'You really want to go ahead?';
+        }
+    }, []);
+
     return players.length <= 0 ? null : (
         <div>
             <Board 
